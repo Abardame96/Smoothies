8c9c5dcc0e0a9db87360faa302ad480e48e03799
A.Abardame <95368979+Abardame96@users.noreply.github.com>
2025-11-19 22:22:49 +0100

    Fix syntax errors in conditional statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,11 +46,11 @@ if ingredients_list:
       
       st.subheader(fruit_chosen + 'Nutrition Information')
       smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
-      if name_on_order == "Kevin" 
+      if name_on_order == "Kevin" :
         order_filled = False
-      if name_on_order == "Divya" 
+      if name_on_order == "Divya" :
         order_filled = True
-      if name_on_order == "Xi" 
+      if name_on_order == "Xi" :
         order_filled = False
       st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
